3fbba19d88fec3d272d06d40de34fead1e968143
tjose56451 <47831711+tjose56451@users.noreply.github.com>
2020-04-05 02:50:57 -0500

    update

---
+++ footer.php
@@ -0,0 +1,69 @@
+<!-- Footer -->
+<footer id="footer">
+				<div class="container">
+					<div class="row">
+						<section class="4u 12u$(medium) about">
+							<h2>Augue eleifend<br />
+							faucibus et cubilia.</h2>
+							<p>Arcu porttitor ut massa adipiscing nascetur varius lorem ipsum ante ac vestibulum lacinia ante praesent. Iaculis porttitor turpis orci arcu et orci. Lobortis non commodo ante lorem ipsum porttitor turpis orci. Arcu porttitor massa lorem ipsum dolor amet consequat etiam.</p>
+							<ul class="actions">
+								<li><a href="#" class="button">Erat vestibulum</a></li>
+							</ul>
+						</section>
+						<section class="4u 6u(medium) 12u$(small)">
+							<h3>Updates</h3>
+							<ul class="updates">
+								<li>
+									<p><a href="#">Ante ac vestibulum lacinia praesent adipiscing nascetur consequat lorem ipsum dolore.</a></p>
+									<span class="timestamp">Posted 2 hours ago</span>
+								</li>
+								<li>
+									<p><a href="#">Mi eu gravida tortor. Commodo lacinia ut ante placerat. In lobortis ac accumsan tortor.</a></p>
+									<span class="timestamp">Posted 4 hours ago</span>
+								</li>
+								<li>
+									<p><a href="#">Erat faucibus lacinia orci vestibulum massa accumsan ac vitae turpis praesent non.</a></p>
+									<span class="timestamp">Posted 8 hours ago</span>
+								</li>
+							</ul>
+						</section>
+						<section class="4u$ 6u$(medium) 12u$(small)">
+							<h3>Get in touch</h3>
+							<ul class="labeled-icons">
+								<li>
+									<h4 class="icon alt fa-home"><span class="label">Address</span></h4>
+									<span>1234 Somewhere Road #80486<br />
+									Nashville, TN 00000</span>
+								</li>
+								<li>
+									<h4 class="icon alt fa-phone"><span class="label">Phone</span></h4>
+									<span>(800) 555-0000 x12345</span>
+								</li>
+								<li>
+									<h4 class="icon alt fa-envelope"><span class="label">Email</span></h4>
+									<a href="#">hello@untitled.tld</a>
+								</li>
+								<li>
+									<h4 class="icon alt fa-facebook"><span class="label">Facebook</span></h4>
+									<a href="#">facebook.com/untitled-tld</a>
+								</li>
+								<li>
+									<h4 class="icon alt fa-twitter"><span class="label">Twitter</span></h4>
+									<a href="#">twitter.com/untitled-tld</a>
+								</li>
+							</ul>
+						</section>
+					</div>
+				</div>
+				<div class="copyright container">
+					<p>&copy; JOSET. 
+                        <script type="text/JavaScript"> var theDate=new Date() document.write(theDate.getFullYear()) </script>
+                        All rights reserved.</p>
+					<ul class="menu">
+						<li><a href="#">Terms of Use</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Contact Us</a></li>
+						<li><a href="#">Refund Policy</a></li>
+					</ul>
+				</div>
+			</footer>
